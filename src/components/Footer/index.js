47a9af7f219679056f843b7c67202943de6b943d
Rakesh Kondi <rakesh.swamy.k@gmail.com>
2021-08-09 15:08:16 +0000

    covid solution

---
+++ src/components/Footer/index.js
@@ -0,0 +1,25 @@
+import './index.css'
+
+const Footer = () => (
+  <div className="footer-section">
+    <h1 className="logo">
+      COVID19<span className="logo-span">INDIA</span>
+    </h1>
+    <p className="title">we stand with everyone fighting on the front lines</p>
+    <div className="icons-container">
+      <img
+        src="https://res.cloudinary.com/dffpcgabr/image/upload/v1628496730/icons/qno7kid7pdmqdq10ayzl.png"
+        alt="follow us github"
+      />
+      <img
+        src="https://res.cloudinary.com/dffpcgabr/image/upload/v1628496709/icons/wam1oqknrbhnymsvu2nt.png"
+        alt="follow us instagram"
+      />
+      <img
+        src="https://res.cloudinary.com/dffpcgabr/image/upload/v1628496745/icons/ogvdkyaalxokhhpyoil6.png"
+        alt="follow us twitter"
+      />
+    </div>
+  </div>
+)
+export default Footer
